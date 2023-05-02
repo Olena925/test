b00727f7b1a2d99d234935cff81316243893f243
Olena <olenavlasenko078@gmail.com>
2023-05-02 13:48:34 +0300

    application

---
+++ 7. 19lessTest.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt("Скільки фільмів ви вже продивились?", "");// так як відповіддю буде число перед promt ми поставили +
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	geners: [],
+	privat: false
+};
+
+const a = prompt("Один з останніх переглянутих фільмів?", ""),
+	b = prompt("На скільки оцінете його?", ""),
+	c = prompt("Один з останніх переглянутих фільмів?", ""),
+	d = prompt("На скільки оцінете його?", "");
+
+
+personalMovieDB.movies[a] = b;   
+personalMovieDB.movies[c] = d;   
+
+console.log(personalMovieDB);
