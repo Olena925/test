11010e5adcbe7065fc889f2b2b94a68e8678309b
Olena <olenavlasenko078@gmail.com>
2023-06-12 16:23:44 +0300

    logikOperator

---
+++ 8. 21 less.js
@@ -0,0 +1,18 @@
+// const hamburger = 5;
+// const frie = 0;
+
+// if(hamburger && frie){
+//     console.log("I am eating");
+// }
+
+// console.log((hamburger && frie));//обернули у подвійні круглі скобки, щоб одразу отримати результат
+
+
+const hamburger = 2;
+const frie = 1;
+
+if(hamburger === 3 && frie){
+	console.log("All eating");
+} else {
+	console.log("We are going");
+}
