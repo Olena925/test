0863ecb4c4a3554776916224556c63165b84b575
Olena <olenavlasenko078@gmail.com>
2023-06-12 17:13:52 +0300

    kombiOperatori

--- 8. 21 less.js
+++ 8. 21 less.js
@@ -8,19 +8,38 @@
 // console.log((hamburger && frie));//обернули у подвійні круглі скобки, щоб одразу отримати результат
 
 
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola === 1 && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(0 && "boubioio");
+
+// if(hamburger === 3 && cola === 1 && fries){
+// 	console.log("All eating");
+// } else {
+// 	console.log("We are going");
+// }
+
+
 const hamburger = 3;
-const fries = 1;
-const cola = 1;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
 
-console.log(hamburger === 3 && cola === 1 && fries);
-
-console.log();
-console.log();
-console.log();
-console.log();
-
-if(hamburger === 3 && cola === 1 && fries){
-	console.log("All eating");
+if(hamburger === 3 && cola ===2 || fries ===3 && nuggets){
+	console.log("All happy");
 } else {
 	console.log("We are going");
 }
+
+console.log(hamburger === 3 && cola ===2 || fries ===3 && nuggets);
+console.log(hamburger || cola || fries);
+
+let johnRepot, alexRepot, samRepot, mariaRepot = "done";
+
+console.log(johnRepot || alexRepot || samRepot || mariaRepot);
